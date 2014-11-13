42b932fba8f09380146d2aecc7ccb09f071e356b
Robert Coscia <coscia@g.havard.edu>
2014-11-13 07:49:53 -0500

    Initial Draft
    Code given when creating a new MVC application

---
+++ CollegeAdvisors/CollegeAdvisors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CollegeAdvisors.Startup))]
+namespace CollegeAdvisors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
